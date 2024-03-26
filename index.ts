fdf709c0e542ebb91eb714f9f82b73a365dcfef9
hammmad-03444 <160322774+hammmad-03444@users.noreply.github.com>
2024-03-26 22:53:56 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const answer=await inquirer.prompt([
+    {message:"Enter Fist Number",type:"number",name:"FirstNumber"},
+    {message:"Enter Second Number",type:"number",name:"SecondNumber"},
+    {message:"Select Your Operator",type:"list",name:"Operator",
+choices:['Addition','Subtraction','Multiplication','Division'],},
+])
+
+if(answer.Operator==='Addition'){
+    console.log(`Your value is ${answer.FirstNumber + answer.SecondNumber} `)
+}
+else if(answer.Operator==='Subtraction'){
+    console.log(`Your value is ${answer.FirstNumber - answer.SecondNumber} `)
+}
+else if(answer.Operator==='Multiplication'){
+    console.log(`Your value is ${answer.FirstNumber * answer.SecondNumber} `)
+}
+else if(answer.Operator==='Division'){
+    console.log(`Your value is ${answer.FirstNumber / answer.SecondNumber} `)
+}
+else{
+    console.log ('please select a valid operator')
+}
